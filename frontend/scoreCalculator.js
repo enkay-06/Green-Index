238d4fd9261f37916497ec73ad2bb3f8e4ed7363
!a7kulshr <!a7kulshr@CEL221104.nexus.uwaterloo.ca>
2025-05-25 18:04:34 -0400

    score calc

--- frontend/scoreCalculator.js
+++ frontend/scoreCalculator.js
@@ -5,4 +5,11 @@ export default function scoreCalculator(){
     const [inputs, setInputs] = useState({});
     const [score, setScore] = useState(null);
 
-    const
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setInputs((prev)=>({...prev, [name]:isNaN(value) ? value:parseFloat(value)}));
+        //copies all previous states, takes current state, checks if it's not a number (converts to float if is)
+    };
+    }
+
+    const handleSubmit = async()=>{}
